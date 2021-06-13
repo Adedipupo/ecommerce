0b2ee16915b5ea24322b090c59b7549c7de42527
Benedict <jokanolaadedipupo@gmail.com>
2021-06-14 00:26:15 +0100

    product detail's action and reducer

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,6 +2,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL 
 } from "../constants/productContants";
 import axios from "axios";
 
@@ -9,12 +12,30 @@ export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
-    const { data } = await axios.get("./api/products");
+    const { data } = await axios.get("/api/products");
 
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data.data });
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/product/${id}`);
+
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data.data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
