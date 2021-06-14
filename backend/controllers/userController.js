4eb9535e70ee18c1142efcb319fddc1be3cab50d
Benedict <jokanolaadedipupo@gmail.com>
2021-06-14 20:36:39 +0100

    user authentication and authorization

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,21 +1,67 @@
-import asyncHandler from "express-async-handler"
-import UserModel from "../models/userModels.js"
+import asyncHandler from "express-async-handler";
+import UserModel from "../models/userModels.js";
+import { generateToken } from "../utils/generateToken.js";
+
+export const createUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await UserModel.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await UserModel.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 
 export const loginUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body
-    
-    const user = await UserModel.findOne({ email })
-     
-    if (user && (await user.matchPassword(password))) { 
-        res.status(200).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-            token: null,
-        })
-    } else {
-        res.status(401)
-        throw new Error('Invalid username or password')
-    }
-})
+  const { email, password } = req.body;
+
+  const user = await UserModel.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid username or password");
+  }
+});
+
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await UserModel.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { loginUser } from "../controllers/userController.js";
+import { createUser, getUserProfile, loginUser } from "../controllers/userController.js";
+import { verifyToken } from "../middleware/auuthMiddleware.js";
 
 const router = express.Router();
 
+router.post("/register", createUser)
 router.post('/login', loginUser)
+router.get('/profile', verifyToken ,getUserProfile)
 
 export default router;
