3b4d177de9b5e7e65715cccd8db4ffcb7f52a9a4
Benedict <jokanolaadedipupo@gmail.com>
2021-06-15 17:21:34 +0100

    user can update profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createUser, getUserProfile, loginUser } from "../controllers/userController.js";
+import { createUser, getUserProfile, loginUser, updateUserProfile } from "../controllers/userController.js";
 import { verifyToken } from "../middleware/auuthMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", createUser)
 router.post('/login', loginUser)
-router.get('/profile', verifyToken ,getUserProfile)
+router.get('/profile', verifyToken, getUserProfile)
+router.put('/profile',verifyToken, updateUserProfile)
 
 export default router;
