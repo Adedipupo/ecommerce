47afcc4b406a0191548ff41498c96a29442d3dca
Benedict <jokanolaadedipupo@gmail.com>
2021-06-15 09:09:08 +0100

    logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from '../constants/userConstant';
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from '../constants/userConstant';
 
 
 export const login = (email,password ) => async (dispatch) => {
@@ -27,4 +27,7 @@ export const login = (email,password ) => async (dispatch) => {
   }
 };
 
-// export default login
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo')
+  dispatch({type: USER_LOGOUT});
+}
