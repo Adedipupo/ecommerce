ca808948b472cc39373307f93edd578b7e9ff393
Benedict <jokanolaadedipupo@gmail.com>
2021-06-16 14:40:46 +0100

    order endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -34,3 +34,23 @@ export const addOrderItems = asyncHandler(async (req, res) => {
       res.status(201).json(createOrder);
   }
 });
+
+export const getAllOrders = asyncHandler(async (req, res) => {
+  const orders = await OrderModel.find({})
+
+  if (orders) {
+    res.status(200).json(orders)
+  } else {
+    res.status(404).json(`No Order Found`)
+  }
+});
+
+export const getOrderById = asyncHandler(async (req, res) => {
+  const order = await OrderModel.findById(req.params.id).populate('user','name email');
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error(`Order not Found`);
+  }
+});
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,13 @@
  import express from "express";
-import { addOrderItems } from "../controllers/orderController.js";
+import { addOrderItems, getAllOrders, getOrderById } from "../controllers/orderController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 
  const router = express.Router();
 
- router.post("/",verifyToken ,addOrderItems);
 
+ router.post("/",verifyToken ,addOrderItems);
+router.get("/", verifyToken, getAllOrders);
+router.get("/:id", verifyToken, getOrderById);
 
  export default router;
