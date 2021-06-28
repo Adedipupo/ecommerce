524044df6198f268aa55bfa8a2e43a7e6d02cfa8
Benedict <jokanolaadedipupo@gmail.com>
2021-06-28 13:10:15 +0100

    upload backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoute from './routes/productRoutes.js'
 import userRoute from './routes/userRoutes.js'
 import orderRoute from './routes/orderRoutes.js'
+import uploadRoute from './routes/uploadRoutes.js'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -17,10 +19,14 @@ const port = process.env.PORT || 5000
 app.use('/api', productRoute)
 app.use('/api/user', userRoute)
 app.use('/api/orders', orderRoute)
+app.use('/api/upload', uploadRoute)
 
 app.get('/api/config/paypal', (req, res) => 
     res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
