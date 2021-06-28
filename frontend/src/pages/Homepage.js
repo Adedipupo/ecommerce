d84e0297c473cfba405aa450aa0e8d2395e91b23
Benedict <jokanolaadedipupo@gmail.com>
2021-06-28 23:52:26 +0100

    pagination in progress

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,39 +1,53 @@
-import React, { useEffect } from 'react'
-import {useDispatch, useSelector } from 'react-redux'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Products/Products'
-import { listProducts } from '../actions/productActions'
-import Loader from '../components/Loader'
-import Message from '../components/Message'
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Products/Products";
+import { listProducts } from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import Paginate from "../components/Paginate";
 
 const Homepage = ({ match }) => {
-    const keyword = match.params.keyword
+  const keyword = match.params.keyword;
 
-    const pageNumber = match.params.pageNumber || 1
+  const pageNumber = match.params.pageNumber || 1;
 
-    const dispatch = useDispatch()
-    
-    const productList = useSelector(state => state.productList)
-    const { loading,error,products} = productList
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(listProducts(keyword,pageNumber))
-    },[dispatch,keyword,pageNumber])
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products, page, pages } = productList;
 
-    return (
+  useEffect(() => {
+    dispatch(listProducts(keyword, pageNumber));
+  }, [dispatch, keyword, pageNumber]);
+
+  return (
+    <>
+      <h1>Latests Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
         <>
-            <h1>Latests Products</h1>
-            {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message>:
-            <Row>
-                {products.map((product)=>(
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product}/>
-                    </Col>
-                ))}
-            </Row>} 
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <div style={{ height: "100px", width: "100vw" }}>
+            <Paginate
+              pages={pages}
+              page={page}
+              keyword={keyword ? keyword : ""}
+            />
+          </div>
         </>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default Homepage
+export default Homepage;
